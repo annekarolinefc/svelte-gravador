ac23de2fcd4cd00442954dff04c2fe6bee1a71a0
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-09-30 08:55:30 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	
+</script>
+
+<main>
+	<h1>Gravador de áudio</h1>
+	<button>Gravar</button> 
+	<button>Parar</button>
+	<p>Aguardando</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		border: 1px solid black;
+	}
+	p{
+		display: inline;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
